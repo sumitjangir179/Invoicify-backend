0959b88276bbeb3376a4968cbe4177545997eea3
sumitjangir179 <sumitjangir179@gmail.com>
2024-11-24 21:19:01 +0530

    refact: change database mongoose to mysql

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,14 @@
-import mongoose from "mongoose";
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize("invoicify", "root", "root", {
+  host: "localhost",
+  dialect: "mysql",
+});
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect();
-    console.log("connection instance", connectionInstance);
+    const connectionInstance = await sequelize.authenticate();
+    console.log("Database connected successfully", connectionInstance);
   } catch (error) {
     console.log("Error in DB connection", error.message);
     process.exit(1);
